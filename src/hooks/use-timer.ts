8b3046dc01702d936bec51df07ac80801314eb30
kozaken <gkozaka4@gmail.com>
2025-08-11 17:00:01 +0900

    fix:タイマーの修正

--- src/hooks/use-timer.ts
+++ src/hooks/use-timer.ts
@@ -1,29 +1,38 @@
 import { useEffect, useState } from 'react';
 
-// タイマーフックを定義
 export const useTimer = () => {
-  // 状態管理（残り時間がtimeで取得できる）
   const [time, setTime] = useState(10);
+  const [isRunning, setIsRunning] = useState(true);
 
-  // timeが変更されるたびに実行される
+  // タイマーリセット
+  const resetTimer = () => {
+    setIsRunning(false);
+    setTime(10);
+    // setTimeout(() => setIsRunning(true), 0);
+  };
+
+  // タイマー停止
+  const pause = () => setIsRunning(false);
+
+  // タイマー再開
+  const resume = () => setIsRunning(true);
+
+  // メインのタイマーロジック
   useEffect(() => {
-    // 0秒以下になったら処理終了（タイマー停止）
-    if (time <= 0) return;
+    if (!isRunning || time <= 0) return;
 
-    // 1秒ごとに減らしていくsetInterval関数id
     const id = setInterval(() => {
-      setTime((prevTime) => {
-        if (prevTime <= 1) {
+      setTime(prev => {
+        if (prev <= 1) {
+          setIsRunning(false);
           return 0;
         }
-        return prevTime - 1;
+        return prev - 1;
       });
     }, 1000);
 
-    // クリーンアップ関数（コンポーネントの削除と合わせてsetIntervalをストップ）
     return () => clearInterval(id);
-  // 第2引数を空配列にすることで、毎回setIntervalが作成されなくなる
-  }, []);
+  }, [isRunning, time]);
 
-  return { time };
+  return { time, resetTimer, pause, resume };
 };
