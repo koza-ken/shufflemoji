85153e2be36e6d495e48825153e3b7a92128cce4
kozaken <gkozaka4@gmail.com>
2025-08-13 17:54:32 +0900

    add:モードの違いをURLでもわかるように、ルーティングの設定

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -27,13 +27,13 @@ export const TopPage = () => {
               <p className="text-2xl font-bold">ゲーム開始</p>
               <div className="flex justify-center gap-4 mt-4">
                 <Link
-                  to="/game"
+                  to="/game/html-css"
                   className="w-40 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
                   HTML/CSS
                 </Link>
                 <Link
-                  to="/game"
+                  to="/game/ruby"
                   className="w-40 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
                   Ruby
