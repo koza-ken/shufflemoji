be81128d791ee95d558bbccc61c1de795719a124
kozaken <gkozaka4@gmail.com>
2025-08-22 15:38:02 +0900

    add:FEの出題シラバス明記

--- word-scramble-nextjs/components/game/FEQuestion.tsx
+++ word-scramble-nextjs/components/game/FEQuestion.tsx
@@ -11,6 +11,7 @@ export const FEQuestion = ({ allChars, handleCharClick }: FEQuestionProps) => {
       <h2 className="text-xl font-semibold text-gray-800 mb-4 text-center">
         <p>文字を並び替えて</p>
         <p>基本情報技術者試験に出てくる略語を作ってください</p>
+        <p className="text-xs text-gray-600 mt-2 text-right">※シラバス Ver.9.1準拠</p>
       </h2>
 
       {/* 利用可能な文字を表示（固定位置） */}
